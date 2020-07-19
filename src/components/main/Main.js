d7e430c1c2c4f99ddae0d98335edc61fa0c14467
Arash Keighobadi <arashkeyghobadi@gmail.com>
2020-07-19 13:52:39 +0200

    Add a form element to accept the user input

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -3,9 +3,37 @@ import MainView from './MainView';
 
 class Main extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            instruction: "Enter the number of blog pages to be scraped:",
+            number_of_pages: '',
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const {name, value} = event.target;
+
+        this.setState(() => {
+            return({[name]: value});
+        });
+    }
+
+    handleSubmit(event) {
+        alert('A number was submitted: ' + this.state.number_of_pages);
+        event.preventDefault();
+    }
+
     render() {
         return(
-            <MainView/>
+            <MainView
+                data={this.state}
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+            />
         );
     }
 }
--- src/components/main/MainView.js
+++ src/components/main/MainView.js
@@ -2,10 +2,29 @@ import React from 'react';
 
 class MainView extends React.Component {
 
+    constructor(props) {
+        super();
+        this.props = props;
+    }
+
     render() {
+        let props = this.props;
+
         return(
             <div style={styles.divStyle}>
-                <p>This is the main content.</p>
+                <form onSubmit={props.handleSubmit}>
+                    <label>
+                        {props.data.instruction}
+                        <input
+                            type='text' 
+                            name='number_of_pages' 
+                            value={props.data.number_of_pages} 
+                            onChange={props.handleChange}
+                            placeholder='Enter a number...'
+                        />
+                    </label>
+                    <input type="submit" value='Submit' />
+                </form>
             </div>
         );
     }
